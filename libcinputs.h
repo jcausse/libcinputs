3fca9725d00bfa481a118f57c1d2699d4d5f5207
Juan Ignacio Causse <juancausse@uca.edu.ar>
2023-07-28 12:21:09 -0300

    Updated header file

--- libcinputs.h
+++ libcinputs.h
@@ -1,6 +1,8 @@
-//
-// Created by Juan Causse on 12/07/2023.
-//
+/*
+ * LibCInputs
+ * libcinputs.h
+ * Created by jcausse - juanignaciocausse@gmail.com
+ */
 
 #ifndef LIBCINPUTS_LIBCINPUTS_H
 #define LIBCINPUTS_LIBCINPUTS_H
@@ -16,22 +18,25 @@
 #include <errno.h>
 #include <limits.h>
 
+//LIBCINPUTS MEMORY MANAGER
 void libcinputs_init(); //TODO
 void libcinputs_cleanup(); //TODO
 
+//INTEGERS
 short parse_short(const char* str, int radix);
 int parse_int(const char* str, int radix);
 long parse_long(const char* str, int radix);
+short getshort(const char* msg, ...); //TODO
+int getint(const char* msg, ...); //TODO
+long getlong(const char* msg, ...); //TODO
 
-//unsigned short parse_u_short(const char* str, int radix);
-//unsigned int parse_u_int(const char* str, int radix);
-//unsigned long parse_u_long(const char* str, int radix);
-
+//FLOATING POINT
 float parsefloat(const char* str); //TODO
 double parsedouble(const char* str); //TODO
-
-int getint(const char* msg, ...); //TODO
+float getfloat(const char* msg, ...); //TODO
 double getdouble(const char* msg, ...); //TODO
+
+//STRINGS
 char* getstring(const char* msg, ...); //TODO
 
 #endif //LIBCINPUTS_LIBCINPUTS_H
